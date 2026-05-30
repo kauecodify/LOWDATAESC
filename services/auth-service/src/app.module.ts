b3c5f73e3dbc8f26ca795e4480d2abd783154758
kauecodify <143859403+kauecodify@users.noreply.github.com>
2026-05-30 01:39:23 -0300

    Update app.module.ts

--- services/auth-service/src/app.module.ts
+++ services/auth-service/src/app.module.ts
@@ -1,4 +1,4 @@
-//nestJS
+//nestJS = core service (skeletons production-ready)
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
